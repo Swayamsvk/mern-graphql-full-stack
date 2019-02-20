79c94b147a8646677ca19ec3448cee216cf18160
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-19 16:00:34 -0800

    Setup graphql server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
+
+//  Setup middleware
+app.use("/graphql", graphqlHTTP({
+    
+}));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
