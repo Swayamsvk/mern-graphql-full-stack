87d4fec8a6900ce4d0b05aab6f57fe45ce39e70d
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-20 14:12:48 -0800

    setting up mongodb as db

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
 const app = express();
+
+//  connect to db
+mongoose.connect(
+  "mongodb+srv://nkhorasi:noor1211@clusterm0-ebmx7.mongodb.net/mern-graphql-full-stack?retryWrites=true",
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 //  Setup middleware
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true })); //    or app.use("/graphql", graphqlHTTP({ schema: schema }));
