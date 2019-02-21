cf654f38315e39e5f4fe891c8911a8975f491cb0
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-20 17:43:50 -0800

    Adding client side using create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Book App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
