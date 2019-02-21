52cf908cb51210f9c75e89d8a5e7769f7124dd38
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-20 18:42:33 -0800

    rendering data from query in component

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 
+//  import the query language to make queries to the server
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 class BookList extends Component {
+  displayBooks() {
+    let data = this.props.data;
+    if (data.loading) {
+      return <div>Loading Books.....</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
     return (
       <div>
-        <ul id="book-list">
-          <li>Book One</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
 
-export default BookList;
+//  stores the data coming from getBooksQuery in this component's props.
+//  so access the data using this.props.
+export default graphql(getBooksQuery)(BookList);
