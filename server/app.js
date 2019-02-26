a08cc4007eb5f16b90cf09782fc169ec8811b98a
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-26 14:19:23 -0800

    more deployment related changes

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ app.use(cors());
 
 //  connect to db
 mongoose.connect(
-  "mongodb+srv://nkhorasi:noor1211@clusterm0-ebmx7.mongodb.net/mern-graphql-full-stack?retryWrites=true",
+  process.env.MONGODB_URI ||
+    "mongodb+srv://nkhorasi:noor1211@clusterm0-ebmx7.mongodb.net/mern-graphql-full-stack?retryWrites=true",
   { useNewUrlParser: true }
 );
 mongoose.connection.once("open", () => {
@@ -22,9 +23,9 @@ mongoose.connection.once("open", () => {
 //  Setup middleware
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true })); //    or app.use("/graphql", graphqlHTTP({ schema: schema }));
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname, "client", "build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
