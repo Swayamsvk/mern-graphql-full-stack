a3aac244e7c51c9d8b8d46b771a056c1cb656334
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-26 17:17:47 -0800

    Version 1.0.0 changes

--- server/app.js
+++ server/app.js
@@ -24,10 +24,10 @@ mongoose.connection.once("open", () => {
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true })); //    or app.use("/graphql", graphqlHTTP({ schema: schema }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
